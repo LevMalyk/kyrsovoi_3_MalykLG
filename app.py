07eb3cc43f3afd3761c901a6c29e7e036fa17070
Lev <tpr.transenergo@gmail.com>
2023-01-24 03:37:56 +1000

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+from main.views import main_blueprint
+from api.views import api_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(main_blueprint)
+app.register_blueprint(api_blueprint)
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return 'Страница не найдена!'
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return 'Ошибка на сервере!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
