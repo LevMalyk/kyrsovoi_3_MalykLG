243bf34426719a7f26bb10aa04267e1df7592832
Lev <tpr.transenergo@gmail.com>
2023-01-25 12:19:34 +1000

    Исправлена система тестирования

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,23 +1,21 @@
 from app import app
 
-
 keys_should_be = {"poster_name",
                   "poster_avatar",
                   "pic",
                   "content",
                   "views_count",
-                  "likes+count",
+                  "likes_count",
                   "pk",
-}
+                  }
 
 
-
-# def test_api_all_posts():
-#     response = app.test_client().get('/api/posts/')
-#     assert response.status_code == 200
-#     api_response = response.json
-#     assert type(api_response) == list
-#     assert set(api_response[0].keys()) == keys_should_be
+def test_api_all_posts():
+    response = app.test_client().get('/api/posts/')
+    assert response.status_code == 200
+    api_response = response.json
+    assert type(api_response) == list
+    assert set(api_response[0].keys()) == keys_should_be
 
 
 def test_api_post():
